b9c1c9f4a4e7ad8f85e1be31a9be09418716aaf4
Ephemerian <106543055+Ephemerian@users.noreply.github.com>
2024-01-31 05:58:46 +0200

    full hw completed

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,16 +1,27 @@
 import { Label } from "../ContactForm/ContactForm.styled";
 import { Wrapper, Field, Input, } from "./Filter.styled";
 
-const Filter = ({handleFilter,value}) => { 
- 
-        return (
-            <Wrapper>
-                <Field>
-                    <Label htmlFor='filter'> Find contacts by name </Label>
-                    <Input onChange={handleFilter} value={value} id='filter' type="text" name="filter" required />
-                </Field>
-            </Wrapper>
-        )
+import { useDispatch, useSelector } from "react-redux";
+
+import { setFilter } from '../../redux/filterSlice';
+
+const Filter = () => {
+    const dispatch = useDispatch();
+
+    const filter = useSelector(state => state.filter);
+
+    const handleFilter = (evt) => {
+        dispatch(setFilter(evt.currentTarget.value.toLowerCase()));
+    }
+
+    return (
+        <Wrapper>
+            <Field>
+                <Label htmlFor='filter'> Find contacts by name </Label>
+                <Input onChange={handleFilter} value={filter} id='filter' type="text" name="filter" required />
+            </Field>
+        </Wrapper>
+    )
 
 }
 
